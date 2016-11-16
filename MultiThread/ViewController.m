c0b835a316d3e44c1b3dce90000755518af7a893
ffm <fengfuming2007@126.com>
2016-11-16 23:32:15 +0800

    NSThread
    test the NSThread

--- MultiThread/ViewController.m
+++ MultiThread/ViewController.m
@@ -29,10 +29,33 @@ void *func()
 }
 
 - (IBAction)clickBtn:(id)sender {
-    pthread_t thread;
-    pthread_create(&thread, NULL, func, NULL);
+//    pthread_t thread;
+//    pthread_create(&thread, NULL, func, NULL);
+    
 //    for (int i = 0; i <5000; i++) {
 //        NSLog(@"%d\n", i);
 //    }
+    NSThread *thread = [[NSThread alloc] initWithTarget:self selector:@selector(run:) object:@"helloThread"];
+    [thread start];
+    if ([NSThread currentThread].isMainThread)
+    {
+        NSLog(@"我就是主线程啊哈哈哈哈");
+    } else
+    {
+        NSLog(@"我才不是主线程呢");
+    }
 }
+
+- (void)run:(NSString *)prarmater
+{
+    NSLog(@"roger the prarmater: %@", prarmater);
+    if ([NSThread currentThread].isMainThread)
+    {
+        NSLog(@"我在run方法 我是主线程");
+    } else
+    {
+        NSLog(@"我在run方法 我不是主线程");
+    }
+}
+
 @end
